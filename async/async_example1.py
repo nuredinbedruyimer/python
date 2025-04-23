c842c900f152d1c4bca9c987c3a931c5eef46dcc
nuredinbedruyimer <nuredinbedru06@gmail.com>
2025-04-23 16:39:27 +0300

    Feat: 'Asynchrounous Programming in Python'

---
+++ async/async_example1.py
@@ -0,0 +1,18 @@
+import asyncio
+
+async def tasks(task_name, delay):
+    print(f"{task_name} Started")
+    #  sleep this function call for 2 second intentionaly before it finishs its task
+    await asyncio.sleep(2)
+    print(f"{task_name} Finished")
+
+async def main():
+    await asyncio.gather(
+        tasks("Call API 1", 2), 
+                tasks("Call API 2", 3)
+,        tasks("Call API 3", 1)
+
+    )
+    
+asyncio.run(main())
+    
